5d11dc141202549b99860f6ac6940c815df2260e
Edcwsyh <Edcwsyh@github.com>
2024-03-13 22:57:47 +0800

    [n][优化部署工程, 将自写脚本作为本工程子项目, 部署脚本新增回显消息]

--- init.sh
+++ init.sh
@@ -22,55 +22,73 @@ my_vim=$vim_dir/myscript
 nvim_parent_dir=~/.config
 
 plug_remote=https://github.com/junegunn/vim-plug
-my_vim_remote=https://github.com/Edcwsyh/My-Vimscript.git
 
 
 if [ ! -d $vim_dir ]
 then
+    echo -e "\e[1;32mSuccess\e[0m : mkdir vim dir : $vim_dir";
     mkdir $vim_dir
 fi
 
 if [ ! -d $autoload_dir ]
 then
+    echo -e "\e[1;32mSuccess\e[0m : mkdir vim auto load dir : $autoload_dir";
     mkdir $autoload_dir
 fi
 
 if [ ! -d $plugged_dir ]
 then
+    echo -e "\e[1;32mSuccess\e[0m : mkdir vim plugged dir : $plugged_dir";
     mkdir $plugged_dir
 fi
 
 if [ -d $plugged_conf_dir ]
 then
+    echo -e "\e[1;32mSuccess\e[0m : delete plugged conf dir";
     rm -rf $plugged_conf_dir
 elif [ -L $plugged_conf_dir ]
 then
+    echo -e "\e[1;32mSuccess\e[0m : delete plugged conf dir link";
     rm $plugged_conf_dir
 fi
 
-if [ ! -d $my_vim ]
+if [ -d $my_vim ]
 then
-    mkdir $my_vim
-    git clone $my_vim_remote $my_vim
+    echo -e "\e[1;32mSuccess\e[0m : delete my vimscript dir";
+    rm -rf $my_vim
+elif [ -L $my_vim ]
+then
+    echo -e "\e[1;32mSuccess\e[0m : delete my vimscript dir link";
+    rm $my_vim
 fi
+
 
 if [ ! -d $nvim_parent_dir ]
 then
+    echo -e "\e[1;32mSuccess\e[0m : mkdir nvim parent dir";
     mkdir $nvim_parent_dir
     ln -s ~/.vim $nvim_parent_dir
 fi
 
+echo -e "\e[1;32mSuccess\e[0m : link ${SCRIPT_DIR}/vimrc to ~/.vimrc";
 ln -Pf ./vimrc ~/.vimrc
+echo -e "\e[1;32mSuccess\e[0m : copy coc-settings.json to ${vim_dir}";
 cp ./coc-settings.json $vim_dir
+echo -e "\e[1;32mSuccess\e[0m : link ${SCRIPT_DIR}/vimrc to ${vim_dir}/init.vim";
 ln -Pf ~/.vimrc $vim_dir/init.vim
 
-ln -s ${SCRIPT_DIR}/plugged.conf ~/.vim/plugged.conf
+echo -e "\e[1;32mSuccess\e[0m : link ${SCRIPT_DIR}/plugged.conf to $plugged_conf_dir";
+ln -s ${SCRIPT_DIR}/plugged.conf $plugged_conf_dir
+echo -e "\e[1;32mSuccess\e[0m : link ${SCRIPT_DIR}/myscript to $my_vim";
+ln -s ${SCRIPT_DIR}/myscript $my_vim
 
 if [ ! -f $autoload_dir/plug.vim ]
 then
+    echo -e "\e[1;32mSuccess\e[0m : init plug.vim";
     git clone $plug_remote $plugged_dir/plug
     cp $plugged_dir/plug/plug.vim $autoload_dir
 fi
 
+echo -e "\e[1;32mSuccess\e[0m : start install YouCompleteMe";
 # install ycm
 ${SCRIPT_DIR}/ycm_manager.sh --install
